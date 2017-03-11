f85029faa8dfee7b42071324915070a14fcfdbe9
Florian Rathgeber <florian.rathgeber@gmail.com>
2017-03-11 18:40:19 +0000

    Enumerate processes starting from 0

--- starter.sh
+++ starter.sh
@@ -4,10 +4,10 @@ set -eux
 
 NPROCS=${NPROCS:-4}
 
-for i in `seq 1 $NPROCS`; do
+for i in `seq 0 $((NPROCS-1))`; do
   systemctl enable phoenix@${i}
 done
 
-for i in `seq 1 $NPROCS`; do
+for i in `seq 0 $((NPROCS-1))`; do
   systemctl start phoenix@${i}
 done
