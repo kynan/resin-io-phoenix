ec713defe7805e37c0f4543fac771fd179953c95
Florian Rathgeber <florian.rathgeber@gmail.com>
2017-03-11 15:21:22 +0000

    Implement /checksum endpoint

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ let checksum = (str, algorithm, encoding) =>
 // reply to request with "Hello World!"
 app.get('/', (req, res) => res.send('Hello World!'));
 
+// Send SHA1 checksum of running application
+// TODO: compute checksum of application image in memory
+app.get('/checksum', (req, res) =>
+  fs.readFile('server.js', (err, data) =>
+    res.send(checksum(data, 'sha1'))
+  )
+);
+
 //start a server on port 80 and log its start to our console
 let server = app.listen(process.env.PORT || 80, () =>
   console.log('Example app listening on port ', server.address().port)
