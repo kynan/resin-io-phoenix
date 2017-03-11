4d11ec28771cf153ba49d933f29803d68b638d96
Florian Rathgeber <florian.rathgeber@gmail.com>
2017-03-11 19:57:42 +0000

    Add local option for starter script

--- starter.sh
+++ starter.sh
@@ -4,10 +4,16 @@ set -eux
 
 NPROCS=${NPROCS:-4}
 
-for i in `seq 0 $((NPROCS-1))`; do
-  systemctl enable phoenix@${i}
-done
+if [[ $(uname -m) == "armv7l" ]]; then
+  for i in `seq 0 $((NPROCS-1))`; do
+    systemctl enable phoenix@${i}
+  done
 
-for i in `seq 0 $((NPROCS-1))`; do
-  systemctl start phoenix@${i}
-done
+  for i in `seq 0 $((NPROCS-1))`; do
+    systemctl start phoenix@${i}
+  done
+else
+  for i in `seq 0 $((NPROCS-1))`; do
+    N=$i ./server.js &
+  done
+fi
